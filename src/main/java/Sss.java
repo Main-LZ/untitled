84a80ef779c5528cec0e2b89bcb63592f8450b9c
Main-LZ <88818765+Main-LZ@users.noreply.github.com>
2021-08-12 18:12:36 +0800

    pull commit

--- src/main/java/Sss.java
+++ src/main/java/Sss.java
@@ -6,5 +6,6 @@ public class Sss {
     public static void main(String[] args) {
         System.out.println("520");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
